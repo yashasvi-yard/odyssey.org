70c3f24f211480cb05233b68426631f99fe7ef96
Yashasvi <yashasvi.coder@outlook.com>
2026-09-01 10:38:48 +0530

    established /upload endpoint

--- main.py
+++ main.py
@@ -4,12 +4,20 @@
 # search?
 # Develop a comprehensive outreach portal that archives expedition reports, scientific datasets, publications, photographs, videos and institutional activities while generating content for websites and social media.
 
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 
 app = FastAPI()
 
 @app.get("/")
-def root():
+async def ping():
     return {
-        "status": "Odyssey begins",
+        "status": "Odyssey begins"
+    }
+
+@app.post("/upload")
+async def upload(file:UploadFile=File(...)):
+    return {
+        "file name":file.filename,
+        "file size":f"{file.size/(1024 * 1024)} MB",
+        "file type":file.content_type
     }
